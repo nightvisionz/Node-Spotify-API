c0ceab17ce47eeb1716d95a8b3354ef44cb26946
Vision <vision@grailbase.com>
2021-01-02 13:25:35 -0600

    updating main

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+var logger = require('morgan'); 
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
